511a5f96a0c93f6342f7d7e7b7fb280fd1a02363
Daniel Malachov <malachovd@seznam.cz>
2019-08-23 10:37:41 +0200

    Create __init__.py

---
+++ predict-it/__init__.py
@@ -0,0 +1,5 @@
+"""Library/framework for making predictions. Choose best of 20 models (ARIMA, regressions, LSTM...). Preprocess data and chose optimal parameters of predictions. Output is plotly HTML interactive graph or database deploying"""
+
+__version__ = '0.1'
+
+from . import main
