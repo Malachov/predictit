5a83a4c0d1e67eab3d466d0e3b0bb20dc0cb4d41
Malachov <malachovd@seznam.cz>
2019-11-07 11:02:24 +0100

    g

--- do/source/conf.py
+++ do/source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import predictit
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,8 +31,12 @@ release = 'r'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc',
+                        'sphinx.ext.intersphinx',
+                        'sphinx.ext.ifconfig',
+                        'sphinx.ext.viewcode',
+                        'sphinx.ext.githubpages',
+                        'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
