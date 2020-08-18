e253aa92dd7ff159617fdb50b2b1fa9e39211705
Dan <malachovd@seznam.cz>
2020-08-18 23:23:25 +0200

    v1.45.3

--- predictit/__init__.py
+++ predictit/__init__.py
@@ -72,7 +72,7 @@ from . import gui_start
 
 from . import main
 
-__version__ = "1.45.2"
+__version__ = "1.45.3"
 __author__ = "Daniel Malachov"
 __license__ = "MIT"
 __email__ = "malachovd@seznam.cz"
