d6760896db28bcbb1ade341c199d955c1a2d5821
Malachov <malachovd@seznam.cz>
2020-08-20 07:56:55 +0200

    Travis correction

--- predictit/__init__.py
+++ predictit/__init__.py
@@ -72,7 +72,7 @@ from . import gui_start
 
 from . import main
 
-__version__ = "1.45.4"
+__version__ = "1.45.5"
 __author__ = "Daniel Malachov"
 __license__ = "MIT"
 __email__ = "malachovd@seznam.cz"
