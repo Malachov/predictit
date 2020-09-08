1fe6cfa4fe389f04f275ef02e385f3838413d95f
Malachov <malachovd@seznam.cz>
2020-09-08 17:05:39 +0200

    Forgot tag

--- predictit/__init__.py
+++ predictit/__init__.py
@@ -72,7 +72,7 @@ from . import gui_start
 
 from . import main
 
-__version__ = "1.50.0"
+__version__ = "1.50.1"
 __author__ = "Daniel Malachov"
 __license__ = "MIT"
 __email__ = "malachovd@seznam.cz"
