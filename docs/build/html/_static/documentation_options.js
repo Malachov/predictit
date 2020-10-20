9fe59e78e4e2a1ad7e7381a19dcba7503b371b8c
Malachov <malachovd@seznam.cz>
2020-10-21 01:05:53 +0200

    Finish rtd

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20-10-2020',
+    VERSION: '21-10-2020',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
